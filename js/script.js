3bf26fac132262f9563fc3bab92066b7e576617f
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2020-06-22 13:33:02 +0300

    WIP

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#name').focus();
+    const roleOther = document.querySelector('#other-title');
+    roleOther.style.display = 'none';
+});
